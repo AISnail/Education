3cc0b864155d63a9df554247a6b5cfcaa2ba967b
pushaowei <542684913@qq.com>
2017-06-18 10:29:08 +0800

    fix: hiden d

--- application/common.php
+++ application/common.php
@@ -8,5 +8,24 @@
 // +----------------------------------------------------------------------
 // | Author: 流年 <liu21st@gmail.com>
 // +----------------------------------------------------------------------
+use Symfony\Component\VarDumper\Dumper\CliDumper;
+use Symfony\Component\VarDumper\Cloner\VarCloner;
+use Symfony\Component\VarDumper\Dumper\HtmlDumper;
 
 // 应用公共文件
+if (! function_exists('dd')) {
+    function dd()
+    {
+        array_map(function ($x) {
+            if (class_exists(CliDumper::class)) {
+                $dumper = 'cli' === PHP_SAPI ? new CliDumper : new HtmlDumper;
+                $dumper->dump((new VarCloner)->cloneVar($x));
+            } else {
+                var_dump($x);
+            }
+        }, func_get_args());
+
+        die(1);
+    }
+}
+
